c110bf6fd1296c2925be8f30ee4b51d0a0133c34
TerekhinaArina <terekhinaarina00@gmail.com>
2022-01-30 12:43:17 +0500

    112233

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// slide home
+// $('.slick-slider').slick({
+//     fade: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     prevArrow: $('.prev__btn'),
+//     nextArrow: $('.next__btn'),
+// })
+
+// $('.slick-slider').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+
+// $('.slick-slider').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 6
+// });
+
+// $('.slick-slider').slick({
+//     infinite: false,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//     prevArrow: $('.prev__arrow'),
+//     nextArrow: $('.next__arrow'),
+// });
+
+// $('.slick-slider').slick({
+//     lazyLoad: 'ondemand',
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     infinite: true,
+//     dots: true,
+//     arrows: true,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     centerMode: true,
+//     responsive: [
+//         {
+//             breakpoint: 576,
+//             settings: {
+//                 arrows: false
+//             }
+//         }
+//     ]
+// });
